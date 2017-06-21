a1aa4f1f4745d965e92f0976224c5bdf2dcc11f7
Katia Patkin <katia.patkin@EECS.Berkeley.EDU>
2017-06-20 23:02:56 -0700

    Fixed batching for super tags + support for dataset generation

--- utils/data_preproc.py
+++ utils/data_preproc.py
@@ -10,121 +10,98 @@ import numpy as np
 import shutil
 
 
-def make_dir(path_dir):
-    
-    if not os.path.exists(path_dir):
-        try:
-            os.makedirs(os.path.abspath(path_dir))
-        except OSError as exc:  # Guard against race condition
-            if exc.errno != errno.EEXIST:
-                raise
-
-
-# TODO: hidden files
-def del_extra_files(root_dir):
-
-    print("[del_extra_files] Deleting mismatching files between dirs")
-    
-    for f in os.listdir(root_dir):
-        if os.path.isdir(os.path.join(root_dir, f)) and f not in ["{:02d}".format(x) for x in xrange(25)]:
-            shutil.rmtree(os.path.join(root_dir, f))
-        if os.path.isfile(os.path.join(root_dir, f)):
-            os.remove(os.path.join(root_dir, f))
-
-
-def get_data(data_path):
-
-    print("[get_data] Getting RAW data from corpora")
-
-    make_dir(data_path)
-    
-    if not os.path.exists(os.path.join(data_path, 'wsj')):
-        os.system("scp -r login.eecs.berkeley.edu:/project/eecs/nlp/corpora/'EnglishTreebank/wsj/ " + data_path)
-        del_extra_files(os.path.join(data_path, 'wsj'))
-
-
 def read_file(fname):
-
     with open(fname) as f:
         text = f.read().splitlines()
-    
     return list(map(lambda x: x.split(), text))
 
+def flatten(list_):
+     for it in list_:
+        for element in it:
+            yield element
 
-def flatten_list(list_):
-    return sum(list_, [])
-        
-
-def build_dataset(words, vocabulary_size = 100000):
-    
-    count = [['UNK', -1]]
-    count.extend(collections.Counter(words).most_common(vocabulary_size - 1))
+def build_dictionary(words, vocabulary_size = -1):
+    count = [['PAD', -1],['UNK', -1]]
+    if vocabulary_size == -1:
+        count.extend(collections.Counter(words).most_common())
+    else:
+        count.extend(collections.Counter(words).most_common(vocabulary_size - 1))
     dictionary = dict()
     for word, _ in count:
         dictionary[word] = len(dictionary)
-    data = list()
     unk_count = 0
     for word in words:
-        if word in dictionary:
-            index = dictionary[word]
-        else:
-            index = 0  # dictionary['UNK']
-        unk_count += 1
-        data.append(index)
-    count[0][1] = unk_count
+        if word not in dictionary:
+            unk_count += 1
+    count[1][1] = unk_count
     reverse_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
-    
-    return data, count, dictionary, reverse_dictionary
-
+    return count, dictionary, reverse_dictionary
 
 def lookup_fn(sentences, dictionary):
-
     vector = []
     for sentence in sentences:
         vector.append(list(map(lambda word: dictionary[word], sentence)))
     return vector
 
-
-def gen_data(path, r_min=0, r_max=24):
-    
-    data = {}
-    count = {}
-    dictionary = {}
-    reverse_dictionary = {}
-    sentences = {}
-    vectors = {}
-               
-    for el in ['tags', 'words']: 
-        in_list = []
-        sentence_list = []
-        for subdir in map(lambda n: str(n).zfill(2), range(r_min, r_max)):
-            current_path = join(path, subdir, el)
-            files_list = list(filter(lambda f: isfile(join(current_path, f)), listdir(current_path)))
-            for f in files_list:
-                text = read_file(join(current_path, f))
-                sentence_list.extend(text)
-                in_list.extend(flatten_list(text))
-        sentences[el] = sentence_list
-        data[el], count[el], dictionary[el], reverse_dictionary[el] = build_dataset(in_list)
-        vectors[el] = lookup_fn(sentences[el], dictionary[el])
-
-    del in_list    # Hint to reduce memory.
-    del sentence_list
-
-    return vectors 
-
-
-def generate_batch(data, batch_size=32):
+def _generate_batch(data, batch_size=32):
     dn = np.array(data)
     dn = np.array_split(dn, len(dn)/batch_size)
     return dn
 
+def generate_batch(data, batch_size=32):
+    batch = dict()
+    d_keys = data.keys()
+    d_size = len(data[d_keys[0]])
+    d_index = np.random.randint(d_size, size=batch_size)
+    for key in d_keys:
+        batch[key] = np.array(data[key])[d_index]
+    return batch
 
-def data_padding(data, pad_sym=0):
-   
-    max_len = len(max(data, key=len))
+def data_padding(data, mlen = 0, pad_sym=0):
+
+    max_len = mlen if mlen>0 else len(max(data, key=len))
     for i,el in enumerate(data):
         pad_len = max_len-len(el)
-        # import pdb; pdb.set_trace()
         data[i] = np.lib.pad(el, (0,pad_len), 'constant', constant_values=(pad_sym))
     return data
+
+def _to_onehot(vec_in, max_len, size):
+    vec_out = np.zeros((max_len, size))
+    vec_out[np.arange(len(vec_in)), vec_in] = 1
+    return vec_out
+
+def gen_sourcefile(text_w, text_t, max_len=10):
+
+    indeces = [i for i in range(len(text_w)) if len(text_w[i])<=max_len]
+    word_list = np.ndarray.tolist(np.array(text_w)[indeces])
+    tag_list = np.ndarray.tolist(np.array(text_t)[indeces])
+
+    with open('word_file', 'w') as f:
+        f.write("\n".join([' '.join(s) for s in word_list]))
+
+    with open('tag_file', 'w') as f:
+        f.write("\n".join([' '.join(s) for s in tag_list]))
+
+    return word_list, tag_list
+
+def gen_dataset(w_file = 'words', t_file = 'tags', max_len=10, tvocab_size=-1, wvocab_size=-1):
+
+    count = dict()
+    dictionary = dict()
+    reverse_dictionary = dict()
+
+    text = read_file(w_file)
+    text_flat = list(flatten(text))
+    count['word'], dictionary['word'], reverse_dictionary['word'] = build_dictionary(text_flat, wvocab_size)
+
+    tags = read_file(t_file)
+    tags_flat = list(flatten([tag.split('+') for tree in tags for tag in tree]))
+    count['tag'], dictionary['tag'], reverse_dictionary['tag'] = build_dictionary(tags_flat, tvocab_size)
+
+    vectors = dict()
+    word_list, tag_list = gen_sourcefile(text, tags, max_len)
+    vectors['word'] = lookup_fn(word_list, dictionary['word'])
+    tag_list_mod = [[tag.split('+') for tag in tree] for tree in tag_list]
+    vectors['tag'] = map(lambda tag_list: lookup_fn(tag_list, dictionary['tag']), tag_list_mod)
+
+    return count, dictionary, reverse_dictionary, vectors
