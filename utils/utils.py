71420b5e9aa1b5c81cba6c0939976ca6f7839055
Katia Patkin <katia.patkin@EECS.Berkeley.EDU>
2017-07-24 10:36:16 -0700

    Refactoring

---
+++ utils/utils.py
@@ -0,0 +1,37 @@
+from __future__ import print_function
+import os
+
+def make_dir(path_dir):
+
+    if not os.path.exists(path_dir):
+        try:
+            os.makedirs(os.path.abspath(path_dir))
+        except OSError as exc: # Guard against race condition
+            if exc.errno != errno.EEXIST:
+                raise
+
+
+def is_balanced(string):
+    iparens = iter('(){}[]<>')
+    parens = dict(zip(iparens, iparens))
+    closing = parens.values()
+    stack = []
+    for ch in string:
+        delim = parens.get(ch, None)
+        if delim:
+            stack.append(delim)
+        elif ch in closing:
+            if not stack or ch != stack.pop():
+                return False
+    return not stack
+
+
+def read_balanced_line(fin):
+    s = ""
+    lines = iter(open(fin, 'r'))
+    for line in lines:
+        if line.strip():
+            s = s + line.split('\n')[0]
+            if is_balanced(s) and s != "":
+                yield s
+                s = ""
