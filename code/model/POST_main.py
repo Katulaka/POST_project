0a0922e89b4430673f17d9d24908509d374c5d41
Katia Patkin <katia.patkin@EECS.Berkeley.EDU>
2017-11-25 18:13:39 -0800

    fix up for empty beam search result

--- code/model/POST_main.py
+++ code/model/POST_main.py
@@ -243,8 +243,8 @@ def decode(config, w_vocab, t_vocab, batcher, t_op, add_pos_in, add_w_pos_in,
             w_len_cp = copy.copy(w_len)
             pos_cp = copy.copy(pos)
             best_beams = bs.beam_search(sess, words_cp, w_len_cp, pos_cp)
-            # import pdb; pdb.set_trace()
             beam_tags = t_op.combine_fn(t_vocab.to_tokens(best_beams['tokens']))
+            import pdb; pdb.set_trace()
             _beam_pair = map(lambda x, y: zip(x, y),
                                             beam_tags,
                                             best_beams['scores'])
