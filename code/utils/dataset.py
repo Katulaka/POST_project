c5e0d3cf3e75cda923234e734ba4b71b6bc1875a
Katia Patkin <katia.patkin@EECS.Berkeley.EDU>
2017-12-13 17:26:25 -0800

    debug

--- code/utils/dataset.py
+++ code/utils/dataset.py
@@ -115,7 +115,7 @@ def gen_dataset(src_dir, data_file, tags_type, max_len,
     for key in dataset.keys():
         dataset[key] = _slice_dataset(dataset[key], max_len[key], min_len[key])
         tags[key] = dataset[key]['tags']
-
+    import pdb; pdb.set_trace()
     start_time = time.time()
     t_op = TagOp(*tags_type)
     for ds in dataset.values():
