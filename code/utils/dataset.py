4ced77867a6a8f08da98005e360e672cbb5029fc
Katia Patkin <katia.patkin@EECS.Berkeley.EDU>
2017-12-28 13:57:17 -0800

    fixed print messages

--- code/utils/dataset.py
+++ code/utils/dataset.py
@@ -11,7 +11,7 @@ from utils import flatten_to_1D, _select
 
 def get_raw_data(data_path):
     """ """
-    print("Getting raw data from corpora")
+    print("[[get_raw_data:]] Getting raw data from corpora")
     if not os.path.exists(data_path):
         try:
             os.makedirs(os.path.abspath(data_path))
@@ -23,16 +23,16 @@ def get_raw_data(data_path):
         "/project/eecs/nlp/corpora/EnglishTreebank/wsj/* ")
         os.system(scp_path + data_path)
 
-def get_data_from_file(src_dir, data_file, get_fn):
+def load_data(src_dir, data_file, get_fn):
     """ """
     start_time = time.time()
     if not os.path.exists(data_file) or os.path.getsize(data_file) == 0:
         data = get_fn(src_dir, data_file)
     else:
-        print ("Data file used: %s" % data_file)
+        print ("[[gen_dataset/load_data:]] File used: %s" % data_file)
         with open (data_file, 'r') as outfile:
             data = json.load(outfile)
-    print("Total time to load data: %f" % (time.time()-start_time))
+    print("[[gen_dataset/load_data:]] %.3fs to load data" % (time.time()-start_time))
     return data
 
 def data_to_dict(src_dir, data_file):
@@ -43,7 +43,7 @@ def data_to_dict(src_dir, data_file):
             data = {'words' : [], 'tags' : []}
             for fname in sorted(filenames):
                 data_in = os.path.join(directory, fname)
-                print("Reading file %s" %(data_in))
+                print("[[load_data/data_to_dict:]] Reading file %s" %(data_in))
                 for tags, words in gen_tags(data_in):
                     data['words'].append(words)
                     data['tags'].append(tags)
@@ -59,6 +59,7 @@ def gold_to_list(gold_src_dir, gold_file):
         if directory[-1].isdigit() and int(directory[-2:])==23:
             for fname in sorted(filenames):
                 data_file = os.path.join(directory, fname)
+                print("[[load_data/gold_to_list:]] Reading file %s" %(data_file))
                 with open(data_file) as f:
                     gold_data += [x.strip('\n') for x in f.readlines()]
     with open(gold_file, 'w') as outfile:
@@ -74,31 +75,32 @@ def split_dataset(data):
     for idx in xrange(2,22):
         dataset['train']['words'].extend(data[str(idx)]['words'])
         dataset['train']['tags'].extend(data[str(idx)]['tags'])
-    print("Total time to split data into train, dev, test: %f" %
+    print("[[gen_dataset/split_dataset:]] %.3fs to split into train/dev/test" %
                                         (time.time()-start_time))
     return dataset
 
-def slice_dataset(dataset, ds_range):
+def slice_dataset(dataset, ds_range, key):
     """ """
     start_time = time.time()
     indeces = [ds_range[0] <= len(w) <= ds_range[1] for w in dataset['words']]
     dataset['words'] = _select(dataset['words'], indeces)
     dataset['tags'] = _select(dataset['tags'], indeces)
-    print("Total time to slice sentences : %f" % (time.time()-start_time))
+    print("[[gen_dataset/slice_dataset(%s):]] %.3fs to keep sentences in range (%s, %s)" %
+                 (key, time.time()-start_time, ds_range[0], ds_range[1]))
     return dataset, indeces
 
 def gen_dataset(config, tags_type, ds_range, nwords=0, ntags=0, nchars=0):
     """ """
-    data = get_data_from_file(config.src_dir, config.ds_file, data_to_dict)
+    data = load_data(config.src_dir, config.ds_file, data_to_dict)
     #split dataset into train/dev/test
     ds = split_dataset(data)
     tags = dict()
     indeces = dict()
     for key in ds.keys():
-        ds[key], indeces[key] = slice_dataset(ds[key], ds_range[key])
+        ds[key], indeces[key] = slice_dataset(ds[key], ds_range[key], key)
         tags[key] = ds[key]['tags']
 
-    gd =  get_data_from_file(config.src_dir, config.gold_file, gold_to_list)
+    gd =  load_data(config.src_dir, config.gold_file, gold_to_list)
     gd = _select(gd, indeces['test'])
 
     start_time = time.time()
@@ -106,7 +108,7 @@ def gen_dataset(config, tags_type, ds_range, nwords=0, ntags=0, nchars=0):
     for d in ds.values():
         d['tags'] = t_op.modify_fn(d['tags'])
         d['chars'] = [[list(w) for w in s] for s in d['words'] ]
-    print ("Total time to modify tags %f" % (time.time()-start_time))
+    print ("[[gen_dataset:]] %.3fs to modify tags" % (time.time()-start_time))
 
     vocab = get_vocab(ds, nwords, ntags, nchars)
 
@@ -114,10 +116,7 @@ def gen_dataset(config, tags_type, ds_range, nwords=0, ntags=0, nchars=0):
     for d in ds.values():
         for k in d.keys():
             d[k] = vocab[k].to_ids(d[k])
-        # d['words'] = vocab['words'].to_ids(d['words'])
-        # d['tags'] = vocab['tags'].to_ids(d['tags'])
-        # d['chars'] = vocab['chars'].to_ids(d['chars']) #TODO
-    print ("Total time to convert data from tokens to ids %f" %
+    print ("[[gen_dataset:]] %.3fs to convert data from tokens to ids" %
             (time.time()-start_time))
     return vocab, ds, t_op, tags, gd
 
@@ -127,17 +126,19 @@ def get_vocab(ds, nwords, ntags, nchars):
     start_time = time.time()
     words = flatten_to_1D(ds['train']['words'])
     vocab['words'] = Vocab(words, nwords)
-    print ("Total time for word vocab %f" % (time.time()-start_time))
+    print ("[[gen_dataset/get_vocab:]] %.3fs for words vocab" %
+                            (time.time()-start_time))
 
     start_time = time.time()
     tags = flatten_to_1D([d['tags'] for d in ds.values()])
     vocab['tags'] = Vocab(tags, ntags)
-    print ("Total time for tag vocab %f" % (time.time()-start_time))
+    print ("[[gen_dataset/get_vocab:]] %.3fs fors tags vocab" %
+                            (time.time()-start_time))
 
     start_time = time.time()
     chars = flatten_to_1D(ds['train']['chars'])
-    # chars = flatten_to_1D([list(w) for s in ds['train']['words'] for w in s])
     vocab['chars'] = Vocab(chars, nchars)
-    print ("Total time for word vocab %f" % (time.time()-start_time))
+    print ("[[gen_dataset/get_vocab:]] %.3fs for chars vocab" %
+                                (time.time()-start_time))
 
     return vocab
