eae83d5175db5fd946af74bdf9c8bfc481895a49
Katia Patkin <katia.patkin@EECS.Berkeley.EDU>
2018-01-02 16:03:06 -0800

    typo

--- code/model/POST_model.py
+++ code/model/POST_model.py
@@ -71,7 +71,7 @@ class POSTModel(object):
 
             self.w_embed_mat = tf.get_variable('word-embeddings',
                             shape=[self.nwords,self.dim_word], dtype=self.dtype,
-                            initializer=tf.contrib.layers.xavier_initializer()))
+                            initializer=tf.contrib.layers.xavier_initializer())
             # w_embed_mat_init = tf.random_uniform([self.nwords, self.dim_word],
             #                                         -1.0, 1.0)
             # self.w_embed_mat = tf.Variable(w_embed_mat_init, name='word-embedding',
