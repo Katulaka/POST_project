0a9e96a862a3add7c9792f4be7039aaf25f720a7
Katia Patkin <katia.patkin@EECS.Berkeley.EDU>
2018-01-05 16:14:03 -0800

    fixed typo in ptb to trees function

--- code/utils/tags/refactor/tree_t.py
+++ code/utils/tags/refactor/tree_t.py
@@ -79,21 +79,21 @@ class TreeT(object):
 
         return True
 
-    def _from_tree_to_ptb(self, nid):
+    def _from_tree_to_ptb(self, nid=self.tree.root):
         # nid = tree.root
         nid = self.tree.subtree(nid).root
         if self.tree[nid].is_leaf():
             return  ' (' + self.tree[nid].tag + ' ' + self.tree[nid].data.word + ')'
 
-        res = ' (' + tree[nid].tag
+        res = ' (' + self.tree[nid].tag
 
         for c_nid in sorted(self.tree.children(nid), key=lambda x: x.identifier):
-            res += self.from_tree_to_ptb(self, c_nid.identifier)
+            res += self._from_tree_to_ptb(self, c_nid.identifier)
 
         return res + ')'
 
-    def from_tree_to_ptb(self):
-        return self._from_tree_to_ptb(self.tree.root)
+    # def from_tree_to_ptb(self):
+    #     return self._from_tree_to_ptb(self.tree.root)
 
     def from_tag_to_tree(self, tag, word, pos_id=0):
         parent_id = None
@@ -210,3 +210,6 @@ class TreeT(object):
             path = {}
             self.tree_to_path(tree.root, path)
             return (self.path_to_tags(path.values()), self.path_to_words(path.keys()))
+
+def trees_to_ptb(trees):
+    return _operate_on_Narray(trees, getattr, 'from_tree_to_ptb')
