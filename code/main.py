f8eb6fabe4d7d74290ab2509b2e56a7d84fb9ceb
Katia Patkin <katia.patkin@EECS.Berkeley.EDU>
2018-01-11 16:50:08 -0800

    clean up main file

--- code/main.py
+++ code/main.py
@@ -16,7 +16,6 @@ def main(_):
     # create vocabulary and array of dataset from train file
     ds = Dataset(config['ds'])
     data = ds.gen_dataset()
-    gold = ds.gen_gold()
     print('==================================================================')
     # initializing batcher class
     batcher = Batcher(**config['btch'])
@@ -41,13 +40,16 @@ def main(_):
         now = datetime.datetime.now().strftime("%Y-%m-%d_%H_%M")
         test_min, test_max = config['ds']['ds_range']['test']
         fname = '_'.join(['ds', str(test_min), str(test_max), now])
-        dec_file = os.path.join('decode', fname + '.test')
-        gold_file = os.path.join('decode', fname + '.gold')
+
         batcher.use_data(ds.dataset['test'])
         decode_trees = model.decode(ds.vocab, batcher, ds.t_op)
         decoded_tags = trees_to_ptb(decode_tags)
+        dec_file = os.path.join('decode', fname + '.test')
         with open(dec_file, 'w') as outfile:
             json.dump(decode_tags, outfile)
+
+        gold = ds.gen_gold()
+        gold_file = os.path.join('decode', fname + '.gold')
         with open(gold_file, 'w') as outfile:
             json.dump(gold, outfile)
 
