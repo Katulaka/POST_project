ddf8456055d1413bec69830f9b4bbd50e4b6118d
Katia Patkin <katia.patkin@EECS.Berkeley.EDU>
2018-02-03 16:38:21 -0800

    debug

--- code/model/refactor/stag_model.py
+++ code/model/refactor/stag_model.py
@@ -206,9 +206,9 @@ class STAGModel(BasicModel):
                 self._add_tag_lstm_layer()
                 if self.config['attn']:
                     self._add_attention()
-                    proj_in = self.dec_out_w_attn
+                    self.pi = proj_in = self.dec_out_w_attn
                 else:
-                    proj_in = self.decode_out
+                    self.pi = proj_in = self.decode_out
                 self._add_projection(proj_in)
                 self._add_loss()
                 if (self.config['mode'] == 'train'):
@@ -247,6 +247,7 @@ class STAGModel(BasicModel):
         if self.config['use_pretrained_pos']:
             input_feed[self.pos_in] = self.pos_step(bv)
         output_feed = [self.loss, self.optimizer] if not dev else [self.loss]
+        import pdb; pdb.set_trace()
         return self.sess.run(output_feed, input_feed)[0]
 
     def train_epoch(self, batcher, dev=False):
