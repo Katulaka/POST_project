1a6b27a7d1edd2a90df9940eea7993a8a79529ef
Katia Patkin <katia.patkin@EECS.Berkeley.EDU>
2018-08-16 01:38:48 -0700

    remove break point

--- rf_code/model/stag_model.py
+++ rf_code/model/stag_model.py
@@ -511,7 +511,6 @@ class STAGModel(BasicModel):
                         break
         s_idx = len(tags)
 
-        import pdb; pdb.set_trace()
         for bv in batcher.get_batch(mode=mode, subset_idx=subset_idx)[s_idx:]:
 
             beams.append(bs.beam_search(self.encode_top_state, self.decode_topk,
