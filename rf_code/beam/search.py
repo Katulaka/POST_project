54be93af2ecd0e801d033a56113a6fa7e301f2a0
Katia Patkin <katia.patkin@EECS.Berkeley.EDU>
2018-08-17 15:44:35 -0700

    move debug

--- rf_code/beam/search.py
+++ rf_code/beam/search.py
@@ -116,12 +116,12 @@ class BeamSearch(object):
 
 
 
+                    import pdb; pdb.set_trace()
                     for hyp in hyps:
                         for j in xrange(self._beam_size):
                             all_hyps.append(hyp.extend_(ids[j],
                                             probs[j],
                                             new_state))
-                    import pdb; pdb.set_trace()
                     # for hyp in hyps:
                     #     latest_token = [[hyp.latest_token]]
                     #     states = hyp.state
