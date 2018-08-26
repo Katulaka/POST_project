7d842c2b627fe6fd890f35dcc852cd0390cc1509
Katia Patkin <katia.patkin@EECS.Berkeley.EDU>
2018-08-25 19:25:45 -0700

    fix typo

--- rf_code/model/stag_model.py
+++ rf_code/model/stag_model.py
@@ -260,7 +260,7 @@ class STAGModel(BasicModel):
                                         self.loss, global_step=self.global_step)
 
     def build_graph(self):
-        with tf.device('/gpu:1')):
+        with tf.device('/gpu:1'):
             # import pdb; pdb.set_trace()
             with tf.Graph().as_default() as g:
                 with tf.variable_scope(self.config['scope_name']):
