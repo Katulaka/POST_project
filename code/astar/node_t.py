83864151359e26bd28af50d1e1d55b28f23616c3
Katia Patkin <katia.patkin@EECS.Berkeley.EDU>
2018-09-01 23:08:03 -0700

    clean up

--- code/astar/node_t.py
+++ code/astar/node_t.py
@@ -20,17 +20,8 @@ class NodeT(object):
             self.tree = [curr_tree]
         if len(self.tree) == 1:
             return True
-        # try:
         combine, r_dst_l_src = can_combine_trees(self.tree, miss_tag_any)
-        # except:
-        #     import pdb; pdb.set_trace()
-
-        # import pdb; pdb.set_trace()
         if combine:
             self.tree = combine_trees(self.tree, miss_tag_any, r_dst_l_src)
             return True
-        # ct = combine_trees(self.tree, miss_tag_any)
-        # if len(ct) == 1 :
-        #     self.tree = ct
-        #     return True
         return False
