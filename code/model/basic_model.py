bb97d2e1490de2c416ae5069f3889fb4cb558b31
Katia Patkin <katia.patkin@EECS.Berkeley.EDU>
2018-10-07 21:07:57 -0700

    clean up

--- code/model/basic_model.py
+++ code/model/basic_model.py
@@ -126,8 +126,6 @@ class BasicModel(object):
         keep_prob = tf.cond(is_training, lambda:dropout, lambda:tf.constant(1.0))
         _cell = tf.contrib.rnn.DropoutWrapper(_cell,
                             output_keep_prob=keep_prob)
-
-                            # variational_recurrent=True)
         return _cell
 
     def _multi_cell(self, nhidden, dropout, is_training, n_layers, is_stack):
